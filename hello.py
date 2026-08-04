df18b47c971ea4151dd25bdc7e72847ad30361ca
motarenan-art <mota.renan@aluno.ifsp.edu.br>
2026-08-04 01:01:45 +0000

    Adiciona as rotas da atividade da Semana 01

--- hello.py
+++ hello.py
@@ -1,11 +1,34 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return '<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/codigostatusdiferente')
+def codigostatusdiferente():
+    return 'Bad request', 400
+
+@app.route('/objetoresposta')
+def objetoresposta():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('meu_cookie', 'valor_teste')
+    return response
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect('https://www.ifsp.edu.br/')
+
+@app.route('/abortar')
+def abortar():
+    abort(404)
